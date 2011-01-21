7417845d8667c31e99e6e539083a4736f0098af0
Hugues Cheguillaume <bagu@bagu.biz>
2011-01-20 17:04:29 -0800

    Correction : error message must be shown only when error happen

--- plugiles/UPP_Mes_uploads.php
+++ plugiles/UPP_Mes_uploads.php
@@ -123,7 +123,7 @@ if(isset($_POST['supprimer']) AND $_POST['supprimer'] != NULL)
 		$url = 'gestionnaire.php';
 	else
 		$url = PLUGIN_URL;
-	if($erreur == 1)
+	if($erreur == 0)
 		redirect($url, $lang_uploadile['delete_success']);
 	else
 		redirect($url, $lang_uploadile['err_delete']);
